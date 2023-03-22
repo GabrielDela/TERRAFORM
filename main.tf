fe18c3cebcf23f0b50f4c826950911b6b484edef
Gabriel <gabrieldelahaye76680@gmail.com>
2023-03-22 10:31:06 +0100

    Add gitignore tf.plan

--- main.tf
+++ main.tf
@@ -1,12 +1,19 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "3.48.0"
     }
   }
 }
 
 provider "azurerm" {
-  # Configuration options
+  features {
+
+  }
+}
+
+resource "azurerm_resource_group" "rg-1" {
+  name     = "rg-pouetpouet"
+  location = "West Europe"
 }
